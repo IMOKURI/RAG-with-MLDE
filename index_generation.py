88e39047bb056ed5a99c5e031656be8ed6e2aa0f
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-11-09 12:10:38 +0900

    feat: update embedding
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

---
+++ index_generation.py
@@ -0,0 +1,76 @@
+import logging
+import os
+
+from determined.pytorch import experimental
+from llama_index import SimpleWebPageReader
+import nest_asyncio
+
+from document_summary_index import CustomDocumentSummaryIndex
+from web_dataset import WebDocument
+
+
+class EmbeddingProcessor(experimental.TorchBatchProcessor):
+    def __init__(self, context):
+        self.rank = context.get_distributed_rank()
+
+        # 非同期処理の有効化
+        nest_asyncio.apply()
+
+        self.document_summary_index = CustomDocumentSummaryIndex(openai_api_base="http://172.19.9.24:8000/v1")
+
+        self.outputs = {}
+        self.last_index = 0
+
+        self.output_dir = "rag-system"
+
+    def process_batch(self, batch, batch_idx) -> None:
+        logging.info(f"Processing batch {batch_idx}")
+        # logging.info(f"Processing batch {batch}")
+
+        documents = []
+        for doc_id, url in zip(batch["doc_id"], batch["url"]):
+            web_doc = SimpleWebPageReader(html_to_text=True).load_data([url])
+            web_doc[0].doc_id = doc_id
+            documents.extend(web_doc)
+
+        logging.info(f"Loaded {len(documents)} documents ...")
+
+        self.document_summary_index.from_documents(documents)
+
+        dir_name = f"worker_{self.rank}_batch_{batch_idx}"
+        self.document_summary_index.persist(os.path.join(self.output_dir, dir_name))
+
+        # self.outputs[batch_idx] = index
+        self.last_index = batch_idx
+
+    def on_checkpoint_start(self):
+        """
+        File names contain rank and batch index information to avoid duplication between:
+        - files created by different workers
+        - files created by the same worker for different batches of input data
+        """
+        if len(self.outputs) == 0:
+            return
+
+        for idx, index in self.outputs.items():
+            ...
+
+        self.outputs = {}
+
+    def on_finish(self):
+        """
+        チーフワーカー (rank 0) で各ワーカーがチェックポイントで保存したファイルを読み込み、マージする。
+        """
+        if self.rank == 0:
+            ...
+
+
+if __name__ == "__main__":
+    dataset = WebDocument("./document_list.csv", size=3)
+
+    experimental.torch_batch_process(
+        EmbeddingProcessor,
+        dataset,
+        batch_size=32,
+        checkpoint_interval=10,
+    )
--- poc_document_summary_index.py
+++ poc_document_summary_index.py
@@ -1,32 +1,13 @@
 import argparse
 import logging
-
-import nest_asyncio
-from llama_index import (
-    DocumentSummaryIndex,
-    OpenAIEmbedding,
-    PromptHelper,
-    ServiceContext,
-    SimpleWebPageReader,
-    StorageContext,
-    get_response_synthesizer,
-    load_index_from_storage,
-)
-from llama_index.graph_stores import SimpleGraphStore
-from llama_index.indices.document_summary import DocumentSummaryIndexLLMRetriever
-from llama_index.llms import OpenAI
-from llama_index.node_parser import SimpleNodeParser
-from llama_index.storage.docstore import SimpleDocumentStore
-from llama_index.storage.index_store import SimpleIndexStore
-from llama_index.text_splitter import TokenTextSplitter
-from llama_index.vector_stores import SimpleVectorStore
-from llama_index.response_synthesizers import ResponseMode
-from llama_index.query_engine import RetrieverQueryEngine
-
-import web_dataset as wds
-import prompt_template as pt
 import time
+
+from llama_index import SimpleWebPageReader
+import nest_asyncio
+
+from document_summary_index import CustomDocumentSummaryIndex
 from utils import time_since
+import web_dataset as wds
 
 
 openai_api_key = "dummy"
@@ -43,91 +24,26 @@ def main():
     # 非同期処理の有効化
     nest_asyncio.apply()
 
-    logging.info(f"Initializing embedding models ... {time_since(start)}")
-    embed_model = OpenAIEmbedding(embed_batch_size=1, api_key=openai_api_key, api_base=openai_api_base)
-
-    logging.info(f"Initializing LLM ... {time_since(start)}")
-    llm = OpenAI(temperature=0, batch_size=1, max_tokens=512, api_key=openai_api_key, api_base=openai_api_base)
-
-    text_splitter = TokenTextSplitter(
-        separator="。", chunk_size=4096, chunk_overlap=64, backup_separators=["、", " ", "\n"]
-    )
-    node_parser = SimpleNodeParser(text_splitter=text_splitter)
-
-    prompt_helper = PromptHelper(
-        context_window=4096, num_output=512, chunk_overlap_ratio=0.05, chunk_size_limit=None, separator="。"
-    )
-
-    service_context = ServiceContext.from_defaults(
-        llm=llm, embed_model=embed_model, node_parser=node_parser, prompt_helper=prompt_helper
-    )
-
-    response_synthesizer = get_response_synthesizer(
-        service_context=service_context,
-        text_qa_template=pt.CHAT_TEXT_QA_PROMPT,
-        summary_template=pt.CHAT_TREE_SUMMARIZE_PROMPT,
-        response_mode=ResponseMode.TREE_SUMMARIZE,
-        use_async=True,
-    )
+    document_summary_index = CustomDocumentSummaryIndex()
 
     if args.build_index:
         ds = wds.WebDocument("./document_list.csv", size=2)
 
-        logging.info(f"Loading documents ... {time_since(start)}")
         documents = []
         for doc_data in ds:
-            logging.info(f"Processing document {doc_data['title']} ... {time_since(start)}")
             web_doc = SimpleWebPageReader(html_to_text=True).load_data([doc_data["url"]])
             web_doc[0].doc_id = doc_data["doc_id"]
             documents.extend(web_doc)
         logging.info(f"Loaded {len(documents)} documents ...")
 
-        logging.info(f"Building index ... {time_since(start)}")
-        index = DocumentSummaryIndex.from_documents(
-            documents,
-            service_context=service_context,
-            response_synthesizer=response_synthesizer,
-            summary_query=pt.SUMMARY_QUERY,
-            show_progress=True,
-            embed_summaries=False,
-        )
+        document_summary_index.from_documents(documents)
+        document_summary_index.persist()
 
-        index.storage_context.persist("./rag-system")
-
-    logging.info(f"Loading index ... {time_since(start)}")
-    # TODO: support multiple indices
-    docstore = SimpleDocumentStore.from_persist_path("./rag-system/docstore.json")
-    indexstore = SimpleIndexStore.from_persist_path("./rag-system/index_store.json")
-    vectorstore = SimpleVectorStore.from_persist_path("./rag-system/vector_store.json")
-    graphstore = SimpleGraphStore.from_persist_path("./rag-system/graph_store.json")
-
-    storage_context = StorageContext.from_defaults(docstore, indexstore, vectorstore, graphstore)
-    doc_summary_index = load_index_from_storage(storage_context)
-
-    # logging.info(f"Getting document summary ... {time_since(start)}")
-    # logging.info(doc_summary_index.get_document_summary("id-003"))
-
-    logging.info(f"Instanciate LLM retriever ... {time_since(start)}")
-    retriever = DocumentSummaryIndexLLMRetriever(
-        doc_summary_index,
-        choice_select_prompt=pt.DEFAULT_CHOICE_SELECT_PROMPT,
-        # choice_batch_size=10,
-        choice_top_k=1,
-        service_context=service_context,
-    )
-
-    logging.info(f"Retrieve document ... {time_since(start)}")
-    retrieved_nodes = retriever.retrieve("Swarm Learning とは何ですか？")
-    logging.info(retrieved_nodes[0])
-    # logging.info(retrieved_nodes[0].node.get_text())
+    document_summary_index.load()
+    document_summary_index.as_retriever()
 
     logging.info(f"Querying document ... {time_since(start)}")
-    query_engine = RetrieverQueryEngine(
-        retriever=retriever,
-        response_synthesizer=response_synthesizer,
-    )
-    response = query_engine.query("Swarm Learning とは何ですか？")
-    logging.info(response)
+    logging.info(document_summary_index.query("Swarm Learning とは何ですか？"))
 
     logging.info(f"Done ... {time_since(start)}")
 
